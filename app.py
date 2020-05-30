41baaad5adb77bae4bedfafe249804e1e53a45b8
Andreea <andrea.iosip@gmail.com>
2020-05-30 19:12:00 +0000

    PyMongo code uncommented

--- app.py
+++ app.py
@@ -7,23 +7,21 @@ from os import path
 if path.exists("env.py"):
     import env
 
+app = Flask(__name__)
 
 MONGO_URI = os.environ.get('MONGO_URI')
 SECRET_KEY = os.environ.get('SECRET_KEY')
-# mongo = PyMongo(app)
 
-app = Flask(__name__)
+mongo = PyMongo(app)
 
 
 @app.route('/')
 @app.route('/index')
-def index(): 
-
+def index():
     return render_template('index.html')
-    
+
 
 if __name__ == '__main__':
-
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
