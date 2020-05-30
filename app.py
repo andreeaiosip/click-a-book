411283bb7fd9a2ea51c093cf131f5c48a3bfdc38
Andreea <andrea.iosip@gmail.com>
2020-05-30 15:18:32 +0000

    Database connection updated

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
+import os
 from flask import Flask
+from flask_pymongo import PyMongo, pymongo
 from os import path
 if path.exists("env.py"):
   import env 
-  
 
+  
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello!</h1>'
+# configuration of Database
+MONGO_URI = os.environ.get(“mongodb%2Bsrv%3A%2F%2Fandreea%3A%3CandreeaDB%3E%40cluster1-2cndy.mongodb.net%2Ftest%3FretryWrites%3Dtrue%26w%3Dmajority”)
 
-    if __name__ == '__main__':
-        app.debug = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+mongo = PyMongo(app)
+
+
+
+if __name__ == '__main__':    
+    app.run(host=os.environ.get('IP'),        
+    port=int(os.environ.get('PORT')),        
+    debug=True)
