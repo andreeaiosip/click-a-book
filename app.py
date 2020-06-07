371220e4dc7b37a1821e3afa03f83d3c072ee0ff
Andreea <andrea.iosip@gmail.com>
2020-06-07 18:17:43 +0000

    New routes created

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 import os
+
 from flask import Flask, render_template, redirect, request, url_for, \
     session, flash, Markup
 from flask_pymongo import pymongo, PyMongo
 from os import path
 
-if path.exists("env.py"):
+if path.exists('env.py'):
     import env
 
 app = Flask(__name__)
 
-MONGO_URI = os.environ.get('MONGO_URI')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# MONGO_URI = os.environ.get('MONGO_URI')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+    
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
