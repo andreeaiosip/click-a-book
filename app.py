e726e5a58f1c587a14888a6e2fb01c19e5599fa3
Andreea <andrea.iosip@gmail.com>
2020-07-01 21:35:13 +0000

    Login form adapted for Flask

--- app.py
+++ app.py
@@ -26,7 +26,25 @@ def index():
         authorsDB=authorsDB,
         )
 
+
+@app.route("/")
+def register():
+    return render_template("/register.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+
