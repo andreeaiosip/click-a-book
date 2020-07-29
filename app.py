c8fff3b351c76ec60e38ed5defccfb5ff91bc5ea
Andreea <andrea.iosip@gmail.com>
2020-07-29 20:10:43 +0000

    Registration form updated

--- app.py
+++ app.py
@@ -1,46 +1,52 @@
 import os
 from os import path
-from flask import Flask, render_template, redirect, request, url_for, \
+if path.exists("env.py"):
+    import env
+from forms import RegistrationForm, LoginForm
+from flask import Flask, flash, render_template, redirect, request, url_for, \
     session, flash, Markup
 from flask_pymongo import PyMongo
 
 # MONGO_URI = os.environ.get("MONGO_URI")
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
-import env
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-print("I'm a db", os.environ.get("MONGO_URI"))
+# print("I'm a db", os.environ.get("MONGO_URI"))
 app.config['SECRET_KEY'] = os.urandom(32)
 # Variables for database
 mongo = PyMongo(app)
+
+# Collections
+
+usersDB = mongo.db.users
+bookInfoDB = mongo.db.bookInfo
 
 
 @app.route('/')
 def index():
     ''' function to display all records on the landing page'''
-    authorsDB = list(mongo.db.authors.find())
-    print(authorsDB)
+    bookInfoDB = list(mongo.db.bookInfo.find())
+    print(bookInfoDB)
     return render_template(
         'index.html',
-        authorsDB=authorsDB,
+        bookInfoDB=bookInfoDB,
         )
 
 
-@app.route("/")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("/register.html")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login")
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
+    form = LoginForm()
+    return render_template("/login.html", title="Login", form=form)
 
 
 if __name__ == '__main__':
