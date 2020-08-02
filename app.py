77cd835e966afd3f447e54798e08dd740960fb3b
Andreea <andrea.iosip@gmail.com>
2020-08-02 23:31:19 +0000

    Profile page created

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def index():
         bookInfoDB=bookInfoDB,
     )
 
-#REGISTER
+# REGISTER
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -52,9 +52,10 @@ def register():
         }
         mongo.db.users.insert_one(register)
 
-         #put the user in session cookie
+         # put the user in session cookie
         session["user"] = request.form.get("username").lower()
-        flash("Registration sucessful")
+        flash("Registration sucessfull")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 # LOGIN-----------------------------------------------------
@@ -71,7 +72,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Nice to see you again, {}".format(request.form.get("username")))
+                    flash("Nice to see you again, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                     # invalid password
                 flash("Incorrect credentials.")
@@ -82,6 +86,15 @@ def login():
             flash("Incorrect credentials")
             return redirect(url_for("login"))
     return render_template('login.html')
+
+
+# USER'S PROFILE
+@app.route("/profile/<username>", methods=['GET', 'POST'])
+def profile(username):
+    # get the session username from db
+    username = mongo.db.users.find_one(
+       {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == '__main__':
