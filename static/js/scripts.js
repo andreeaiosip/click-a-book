ac723d744b040ed1115f5cff85e9bf3a8dc1575a
Andreea <andrea.iosip@gmail.com>
2020-08-11 16:29:59 +0000

    Flash messages disappear after 2 sec

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,12 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+// Timeout Flash: handles multiple Flask flash messages.
+setTimeout(()=> {
+    flash_message = document.getElementsByClassName("flash-message");
+    for (let i = 0; i < flash_message.length; i++) {
+        flash_message[i].style.display="none";
+    }
+}, 2000);
