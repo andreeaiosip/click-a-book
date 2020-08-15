f8b909b82305cbde17ec7b3b479dfbff7aea43a4
Andreea <andrea.iosip@gmail.com>
2020-08-15 18:52:45 +0000

    Comments added

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['SECRET_KEY'] = os.urandom(32)
 
 mongo = PyMongo(app)
 
-# Display all books and comments on home page
+# Taking the books and comments table and display the data on home page 
 @app.route('/')
 def index():
     ''' function to display all books on the home page'''
@@ -29,18 +29,18 @@ def register():
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-
+# If the username already exist a flash message will notify the user
         if existing_user:
             flash("Username already taken")
             return redirect(url_for("register"))
-
+# Take user's username and password and save in database
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
-         # put the user in session cookie
+         # Save user's details into the session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration sucessfull")
         return redirect(url_for("index", username=session["user"]))
@@ -55,7 +55,7 @@ def login():
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            # password match check
+            # Check if the password matches with the password from the database for that user
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
@@ -64,24 +64,25 @@ def login():
                 return redirect(url_for(
                         "index", username=session["user"]))
             else:
-                # invalid password
+                # If the password is not valid a flash message will inform the user of invalid credentials
                 flash("Incorrect credentials.")
                 return redirect(url_for("login"))
 
         else:
-            # the username is not registered
+            # If the username is not registered a flash message will inform the 
+            # user of invalid credentials
             flash("Incorrect credentials")
             return redirect(url_for("login"))
     return render_template('login.html')
 
 
-# Userțs Profile -----------------------
+# User Profile Page -----------------------
 @app.route("/profile/<username>", methods=['GET', 'POST'])
 def profile(username):
-    # get the session username from db
+    # Get the session user's username from the database 
     username = mongo.db.users.find_one(
        {"username": session["user"]})["username"]
-
+# Display user's username on the page
     if session["user"]:
         return render_template("profile.html", username=username)
 
@@ -92,8 +93,7 @@ def profile(username):
 
 @app.route("/logout")
 def logout():
-    # remove user from current session cookie
-
+    # Remove user from current session cookie 
     flash("You have been logged out. See you soon!")
     session.pop("user")
     return redirect(url_for("login"))
@@ -127,7 +127,7 @@ def add_comment(book_id):
     return render_template("add_comment.html", book=book)
 
 
-# DELETE COMMENT -----------------
+# Delete comment -----------------
 @app.route("/delete_comment/<comment_id>", methods=["GET", "POST"])
 def delete_comment(comment_id):
     mongo.db.comments.remove({"_id": ObjectId(comment_id)})
