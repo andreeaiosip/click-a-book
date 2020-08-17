672f2fcb8540e9081bf5bd6a802650ac7d96107a
Andreea <andrea.iosip@gmail.com>
2020-08-17 19:10:02 +0000

    Comments added to the code

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['SECRET_KEY'] = os.urandom(32)
 
 mongo = PyMongo(app)
 
-# Taking the books and comments table and display the data on home page 
+# Taking the books and comments table and display the data on home page
 @app.route('/')
 def index():
     ''' function to display all books on the home page'''
@@ -22,7 +22,7 @@ def index():
     comments = list(mongo.db.comments.find())
     return render_template('index.html', books=books, comments=comments)
 
-# Register ------------------------------------
+# Register Page
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -39,13 +39,14 @@ def register():
         }
         mongo.db.users.insert_one(register)
 
-         # Save user's details into the session cookie
+        # Save user's details into the session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration sucessfull")
         return redirect(url_for("index", username=session["user"]))
     return render_template("register.html")
 
-# Login -----------------------------------------------------
+
+# Login Page
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -54,7 +55,8 @@ def login():
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            # Check if the password matches with the password from the database for that user
+            # Check if the password matches with the password
+            # from the database for that user
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
@@ -63,62 +65,67 @@ def login():
                 return redirect(url_for(
                         "index", username=session["user"]))
             else:
-                # If the password is not valid a flash message will inform the user of invalid credentials
+                # If the password is not valid a flash message
+                # will inform the user of invalid credentials
                 flash("Incorrect credentials.")
                 return redirect(url_for("login"))
 
         else:
-            # If the username is not registered a flash message will inform the 
+            # If the username is not registered a flash message will inform the
             # user of invalid credentials
             flash("Incorrect credentials")
             return redirect(url_for("login"))
     return render_template('login.html')
 
 
-# User Profile Page -----------------------
+# User Profile Page
 @app.route("/profile/<username>", methods=['GET', 'POST'])
 def profile(username):
-    # Get the session user's username from the database 
+    # Get the session user's username from the database
     username = mongo.db.users.find_one(
        {"username": session["user"]})["username"]
-# Display user's username on the page
+    # Display user's username on the page
     if session["user"]:
         return render_template("profile.html", username=username)
 
     return redirect(url_for("profile"))
 
 
-# Logout -----------------------------------
+# Logout
 
 @app.route("/logout")
 def logout():
-    # Remove user from current session cookie 
+    # Remove user from current session cookie
     flash("You have been logged out. See you soon!")
     session.pop("user")
     return redirect(url_for("login"))
 
 
-# DELETE PROFILE ------------------------------
+# Delete profile
 
 @app.route("/delete-profile/<user_id>", methods=["GET", "POST"])
 def delete_profile(user_id):
+    # Take the session user username and removes from database
     mongo.db.users.remove({"username": session["user"]})
+    # Clears the cache after the username has been deleted
     session.clear()
     flash("Your profile has been deleted.")
     return redirect(url_for("index"))
 
 
-# Add comment -------------------------
+# Add comment to the books
 @app.route("/add-comment/<book_id>", methods=["GET", "POST"])
 def add_comment(book_id):
+    # Get the id of the book for which we want to comment
     book = mongo.db.bookInfo.find_one({"_id": ObjectId(book_id)})
     if request.method == "POST":
+        # New comment is saved in the correct format for the comments table
         new_comment = {
             "title": book["title"],
             "comment": request.form.get("comment"),
             "username": session["user"]
         }
-
+        # New comment is added to the comments table
         mongo.db.comments.insert_one(new_comment)
         flash("Comment added")
         return redirect(url_for("index"))
@@ -126,19 +133,21 @@ def add_comment(book_id):
     return render_template("add-comment.html", book=book)
 
 
-# Delete comment -----------------
+# Delete comment
 @app.route("/delete-comment/<comment_id>", methods=["GET", "POST"])
 def delete_comment(comment_id):
+    # Remove the comment by using the comment id
     mongo.db.comments.remove({"_id": ObjectId(comment_id)})
     flash("Comment deleted")
     return redirect(url_for("index"))
 
 
-# Update comment  -------------------------
+# Update comment
 @app.route("/update-comment/<comment_id>", methods=["GET", "POST"])
 def update_comment(comment_id):
     comments = mongo.db.comments.find_one({"_id": ObjectId(comment_id)})
     if request.method == "POST":
+        # The comment found by id is updated with the new comment
         mongo.db.comments.update({'_id': ObjectId(comment_id)}, {
             "title": comments["title"],
             "comment": request.form.get("comment"),
